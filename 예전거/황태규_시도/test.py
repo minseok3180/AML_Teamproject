f4331b358788ac30788f2879bb67eda705468b91
minseok3180 <als3180@gmail.com>
2025-06-23 13:56:33 +0900

    정리

--- 예전거/황태규_시도/test.py
+++
@@ -1,8 +0,0 @@
-import torch
-import numpy as np
-
-print(np.__version__)
-print(torch.__version__)
-print(torch.version.cuda)
-print(torch.backends.cudnn.version())
-print(torch.cuda.is_available())
